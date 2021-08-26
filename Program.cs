d299345b0d9664068f535ff35768788d2bc85490
Mehmet Bekir UNAT <81906591+mehmetbekirunat@users.noreply.github.com>
2021-08-26 08:46:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
 
+using QuizAutomation.Utils.Data;
 
 namespace QuizAutomation
 {
@@ -15,67 +17,92 @@ namespace QuizAutomation
         {
             EdgeDriver edgeDriver = new EdgeDriver();
 
-            IJavaScriptExecutor javascriptExecutor = (IJavaScriptExecutor)edgeDriver;
-            javascriptExecutor.ExecuteScript("window.open('http://bilinch.net/hans/home.php', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,width=device-width,height=device-height')");
-            
-            try
+            IJavaScriptExecutor javascriptExecutor = edgeDriver;
+            javascriptExecutor.ExecuteScript("window.open('', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,width=device-width,height=device-height')");
+
+            edgeDriver.SwitchTo().Window(edgeDriver.WindowHandles[1]);
+
+            edgeDriver.Url = "http://bilinch.net/hans";
+
+            edgeDriver.FindElementByXPath("//*[@id=\"username\"]").SendKeys("admin");
+            edgeDriver.FindElementByXPath("//*[@id=\"password\"]").SendKeys("admin123" + Keys.Enter);
+
+            Thread.Sleep(100);
+
+            edgeDriver.Url = "http://bilinch.net/hans/questions.php";
+
+            PropertiesFile dataProperties = Properties.GetPropertiesFileLocally("data.properties");
+
+            int page = int.Parse(dataProperties.GetValue("page"));
+
+            while (true)
             {
-                edgeDriver.SwitchTo().Window(edgeDriver.WindowHandles[1]);                
-                
-                edgeDriver.Url = "http://bilinch.net/hans/";
+                DateTime now = DateTime.Now;
 
-                edgeDriver.FindElementByXPath("//*[@id=\"username\"]").SendKeys("admin");
-                edgeDriver.FindElementByXPath("//*[@id=\"password\"]").SendKeys("admin123" + Keys.Enter);
-
-                edgeDriver.SwitchTo().Window(edgeDriver.WindowHandles[0]);
-
-                for (int i = 2; i <= 26; i++)
+                for (int i = int.Parse(dataProperties.GetValue("index")); i <= 26; i++)
                 {
-                    edgeDriver.Url = "https://milyonist.com/tv/milyoner/soru/listele/maddi/15bin";
-                    
-                    string address = edgeDriver.FindElementByXPath($"/html/body/div[1]/article/div[1]/div/div[{i}]/div[2]/a").GetAttribute("href");
-                    edgeDriver.Url = address;
+                    edgeDriver.SwitchTo().Window(edgeDriver.WindowHandles[0]);
+
+                    edgeDriver.Url = $"https://milyonist.com/tv/milyoner/soru/listele/maddi/60bin?sayfa={page}";
+                    edgeDriver.Url = edgeDriver.FindElementByXPath($"/html/body/div[1]/article/div[1]/div/div[{i}]/div[2]/a").GetAttribute("href");
 
                     try
                     {
-                        edgeDriver.FindElementByXPath("//*[@id=\"Layer_1\"]").Text.Replace("\u00AD", "");
-                        Console.WriteLine("There is voice!");
+                        edgeDriver.FindElementById("sesli");
+                        Console.WriteLine("Voice found, skipped!");
                     }
-                    catch
+                    catch (NoSuchElementException)
                     {
-                        string question = edgeDriver.FindElementByXPath("/html/body/div[1]/article/div[1]/div[1]/section[1]/div[2]/p").Text.Replace("\u00AD", "");
-                        
+                        string question = edgeDriver.FindElementByClassName("Question__text").Text.Replace("\u00AD", "");
                         Console.WriteLine($"QUESTION: {question}");
 
-                        IWebElement webElement;
+                        IReadOnlyCollection<IWebElement> webElements = edgeDriver.FindElementsByClassName("Multiple__choices");
+
+                        List<string> choices = new List<string>();
                         string rightChoice = "";
-
-                        for (int i2 = 1; i2 <= 2; i2++)
+                        
+                        foreach (IWebElement webElement in webElements)
                         {
-                            for (int i3 = 1; i3 <= 2; i3++)
-                            {
-                                webElement = edgeDriver.FindElementByXPath($"/html/body/div[1]/article/div[1]/div[1]/section[1]/div[3]/div[{i2}]/div[{i3}]/div");
+                            string choice = webElement.FindElement(By.ClassName("Multiple__letter")).Text;
+                            string answer = webElement.FindElement(By.ClassName("Multiple__text")).Text;
 
-                                string choice = webElement.FindElement(By.CssSelector(".Multiple__letter")).Text;
-                                string answer = webElement.FindElement(By.CssSelector(".Multiple__text")).Text;
+                            choices.Add(answer);
 
-                                Console.WriteLine($"{choice}: {answer}");
+                            Console.WriteLine($"{choice}: {answer}");
 
-                                if (webElement.GetAttribute("class").Contains("Multiple__right_answer"))
-                                {
-                                    rightChoice = choice;
-                                    // TODO: select right choice
-                                }
-                            }
+                            if (webElement.GetAttribute("class").Contains("Multiple__right_answer"))
+                                rightChoice = choice;
                         }
 
                         Console.WriteLine($"RIGHT CHOICE: {rightChoice}");
+
+                        edgeDriver.SwitchTo().Window(edgeDriver.WindowHandles[1]);
+
+                        new SelectElement(edgeDriver.FindElementByXPath("//*[@id=\"category\"]")).SelectByValue("9");
+
+                        edgeDriver.FindElementByXPath("//*[@id=\"question\"]").SendKeys(question);
+
+                        for (int cI = 0; cI < choices.Count; cI++)
+                        {
+                            string choice = cI == 0 ? "a" : (cI == 1 ? "b" : (cI == 2 ? "c" : (cI == 3 ? "d" : "")));
+                            edgeDriver.FindElementByXPath($"//*[@id=\"{choice}\"]").SendKeys(choices[cI]);
+                        }
+
+                        edgeDriver.FindElementByXPath("//*[@id=\"level\"]").SendKeys("3");
+
+                        new SelectElement(edgeDriver.FindElementByXPath("//*[@id=\"answer\"]")).SelectByValue(rightChoice.ToLower());
+
+                        edgeDriver.FindElementByXPath("//*[@id=\"submit_btn\"]").Click();
                     }
+
+                    dataProperties.SetAndSave("index", (i != 26 ? i + 1 : 2) + "");
                 }
-            }
-            finally
-            {
-                edgeDriver.Quit();
+
+                dataProperties.SetAndSave("page", ++page + "");
+
+                TimeSpan elapsed = DateTime.Now - now;
+
+                Console.WriteLine($"{elapsed.Minutes} minutes/{elapsed.Seconds} seconds passed!");
             }
         }
     }
